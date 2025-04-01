dc5a248d0ed8f96672196297f04173725a329bb9
adam <brown-a2@users.noreply.github.com>
2025-04-01 14:34:09 +0100

    Add some colour tests

--- patterns/header.php
+++ patterns/header.php
@@ -1,24 +1,30 @@
 <?php
 /**
  * Title: Header
- * Slug: govwind/header Categories: header
+ * Slug: govwind/header
+ * Categories: header
  * Block Types: core/template-part/header
  * Description: Header with site title and navigation.
  *
  * @package WordPress
- * @subpackage Govwind 
+ * @subpackage Govwind
  * @since Govwind 0.1.0
  */
 
 ?>
 <!-- wp:group {"align":"full","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull bg-mint-500">
+	<!-- wp:heading {"level":1} -->
+	<h1 class="wp-block-heading">2ajsdlfkjsadlkf</h1>
+	<!-- /wp:heading -->
+
 	<!-- wp:group {"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group">
-		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
-			<!-- wp:site-title {"level":0} /-->
-			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--30)","bottom":"var(--wp--preset--spacing--30)"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30); padding-bottom:var(--wp--preset--spacing--30);">
+			<!-- wp:site-title /-->
+			
+			<!-- wp:group {"style":{"spacing":{"blockGap":"var(--wp--preset--spacing--10)"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 			<div class="wp-block-group">
 				<!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /-->
 			</div>
@@ -29,3 +35,4 @@
 	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
+

d39ad6e1c8df48547772b07c957a0776bbf9e846
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2025-04-11 09:39:39 +0100

    Maxwidths for header,content,footer

--- patterns/header.php
+++ patterns/header.php
@@ -11,28 +11,34 @@
  * @since Govwind 0.1.0
  */
 
+ 
 ?>
 <!-- wp:group {"align":"full","layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull bg-mint-500">
-	<!-- wp:heading {"level":1} -->
-	<h1 class="wp-block-heading">2ajsdlfkjsadlkf</h1>
-	<!-- /wp:heading -->
 
-	<!-- wp:group {"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
-		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--30)","bottom":"var(--wp--preset--spacing--30)"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30); padding-bottom:var(--wp--preset--spacing--30);">
-			<!-- wp:site-title /-->
-			
-			<!-- wp:group {"style":{"spacing":{"blockGap":"var(--wp--preset--spacing--10)"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-			<div class="wp-block-group">
-				<!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /-->
+	<!-- wp:group -->
+	<div class="wp-block-group max-w-screen-xl mx-auto pr-4 pl-4 md:pr-0 md:pl-0">
+		<!-- wp:heading {"level":1} -->
+		<h1 class="wp-block-heading">Header 3</h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--30)","bottom":"var(--wp--preset--spacing--30)"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+			<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30); padding-bottom:var(--wp--preset--spacing--30);">
+				<!-- wp:site-title /-->
+				
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var(--wp--preset--spacing--10)"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+				<div class="wp-block-group">
+					<!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /-->
+				</div>
+				<!-- /wp:group -->
 			</div>
 			<!-- /wp:group -->
 		</div>
 		<!-- /wp:group -->
 	</div>
-	<!-- /wp:group -->
+	<!-- /wp:group -->	
 </div>
 <!-- /wp:group -->
 

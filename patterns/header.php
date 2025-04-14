a81c755e22a6bf95705600142d26f73a27651fe0
Malcolm.Butler <malcolm.butler@digital.justice.gov.uk>
2025-04-14 14:46:17 +0100

    footer mainly

--- patterns/header.php
+++ patterns/header.php
@@ -4,22 +4,21 @@
  * Slug: govwind/header
  * Categories: header
  * Block Types: core/template-part/header
- * Description: Header with site title and navigation.
+ * Description: Header with site title.
  *
  * @package WordPress
  * @subpackage Govwind
  * @since Govwind 0.1.0
  */
-
- 
 ?>
-<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull bg-yellow-500">
+
+<!-- wp:group {"tagName":"header","align":"full","layout":{"type":"default"}} -->
+<header class="wp-block-group alignfull bg-yellow-500 grid grid-cols-[12px_auto_12px] lg:grid-cols-[minmax(12px,_auto)_1000px_minmax(12px,_auto)] col-span-3">
 
 	<!-- wp:group -->
-	<div class="wp-block-group max-w-screen-xl mx-auto pr-4 pl-4 md:pr-0 md:pl-0">
+	<div class="wp-block-group max-w-screen-xl col-start-2">
 		<!-- wp:heading {"level":1} -->
-		<h1 class="wp-block-heading">Header 3</h1>
+		<h1 class="wp-block-heading">Header 1 qweqwe</h1>
 		<!-- /wp:heading -->
 		<!-- wp:heading {"level":2} -->
 		<h2 class="wp-block-heading">Header 2</h2>
@@ -30,18 +29,12 @@
 			<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--30)","bottom":"var(--wp--preset--spacing--30)"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 			<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30); padding-bottom:var(--wp--preset--spacing--30);">
 				<!-- wp:site-title /-->
-				
-				<!-- wp:group {"style":{"spacing":{"blockGap":"var(--wp--preset--spacing--10)"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-				<div class="wp-block-group">
-					<!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /-->
-				</div>
-				<!-- /wp:group -->
 			</div>
 			<!-- /wp:group -->
 		</div>
 		<!-- /wp:group -->
 	</div>
 	<!-- /wp:group -->	
-</div>
+</header>
 <!-- /wp:group -->
 

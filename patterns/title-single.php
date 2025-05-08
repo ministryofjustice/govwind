9162ac57e2e98599c8952bd8e34ba996a17d8388
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-05-08 12:15:10 +0100

    Added single template and heading h1 pattern

---
+++ patterns/title-single.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Title-single
+ * Slug: govwind/title-single
+ * Categories: title
+ * Block Types: core/template-part/titlesingle
+ * Description: Page title.
+ *
+ * @package WordPress
+ * @subpackage Govwind
+ * @since Govwind 0.1.0
+ */
+?>
+
+<!-- wp:heading {"level":1,"align":"left"} -->
+<h1 class="wp-block-heading col-start-2 text-5xl">
+	<?php single_post_title(); ?>
+</h1>
+<!-- /wp:heading -->

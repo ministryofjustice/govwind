ddb67c71a8dbaca00425505aa65710fb5181fa2d
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-06-02 08:41:12 +0100

    home and search

--- patterns/404.php
+++ patterns/404.php
@@ -7,13 +7,12 @@
 ?>
 
 <!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading" id="page-not-found">
+<h1 class="wp-block-heading col-start-2" id="page-not-found">
 	Page Not found
 </h1>
 <!-- /wp:heading -->
 <!-- wp:paragraph -->
-<p class="">
+<p class="col-start-2">
 	The page you are looking for does not exist, or it has been moved. 
 </p>
 <!-- /wp:paragraph -->
-<!-- wp:pattern {"slug":"twentytwentyfour/hidden-search"} /-->
---
+++ patterns/search.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Search
+ * Slug: govwind/hidden-search
+ * Inserter: no
+ *
+ * @package WordPress
+ */
+
+?>
+
+<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"","buttonText":"Search"} /-->

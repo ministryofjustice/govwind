0ce702f8a28315b034245446948100b98da5cec9
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-07-04 09:34:56 +0100

    New pattern, mobile tweaks

--- patterns/footer.php
+++ patterns/footer.php
@@ -21,13 +21,6 @@
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group {"className":"text-center lg:inline-block mx-auto lg:mx-0 w-[150px] pt-3"} -->
-    <div class="wp-block-group text-center lg:inline-block mx-auto lg:mx-0 w-[150px] pt-3">
-        <!-- wp:safe-svg/svg-icon {"svgURL":"https://hale.docker/playground/wp-content/uploads/sites/2/2025/06/royal-arms-5.svg","imageID":1534,"imageWidth":702,"imageHeight":624,"dimensionWidth":176,"dimensionHeight":156,"imageSizes":{"full":{"file":"royal-arms-5.svg","width":702,"height":624,"mime_type":"image/svg+xml","source_url":"https://hale.docker/playground/wp-content/uploads/sites/2/2025/06/royal-arms-5.svg"},"medium":{"width":"300","height":"300","crop":false,"file":"royal-arms-5.svg","mime_type":"image/svg+xml","source_url":"https://hale.docker/playground/wp-content/uploads/sites/2/2025/06/royal-arms-5.svg"},"thumbnail":{"width":"150","height":"150","crop":"1","file":"royal-arms-5.svg","mime_type":"image/svg+xml","source_url":"https://hale.docker/playground/wp-content/uploads/sites/2/2025/06/royal-arms-5.svg"}},"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->
-        <!-- wp:paragraph -->
-        <p>©Crown Copyright</p>
-        <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+    <!-- wp:template-part {"slug":"crown-copyright","theme":"govwind"} /-->
 </div>
 <!-- /wp:group -->

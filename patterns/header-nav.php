7fd999947a6048dcda0b065dbbf646c7db0279ff
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-07-30 11:54:34 +0100

    Added 4 header options

---
+++ patterns/header-nav.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Header with navigation
+ * Slug: govwind/header-nav
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Site header with a navigation bar.
+ *
+ * @package WordPress
+ * @subpackage Govwind
+ * @since Govwind 0.1.0
+ */
+
+?>
+<!-- wp:group {"metadata":{"categories":["header"],"patternName":"govwind/header","name":"Header"},"align":"full","className":"has-white-color has-text-color col-start-2 py-3 helloCCRC has-white-color has-text-color has-link-color","backgroundColor":"vivid-cyan-blue"} -->
+<div class="wp-block-group alignfull col-start-2 py-3 helloCCRC has-white-color has-text-color has-link-color has-vivid-cyan-blue-background-color has-background"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":145,"shouldSyncIcon":false} /-->
+
+<!-- wp:site-title /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":1552} /--></div>
+<!-- /wp:group -->

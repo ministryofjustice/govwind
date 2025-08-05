40fd755fc4e1ec134712f04d554d66227f8f9838
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-08-05 13:45:07 +0100

    Renamed files, footer tag and tweaks

---
+++ patterns/header-1-nav-inline.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Header with inline navigation
+ * Slug: govwind/header-nav-inline
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: Header with navigation bar in line with the title and logo.
+ *
+ * @package WordPress
+ * @subpackage Govwind
+ * @since Govwind 0.1.0
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["header"],"patternName":"govwind/header-test","name":"Header"},"align":"full","className":"col-start-2 py-3 has-white-color has-text-color ","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"luminous-vivid-amber","textColor":"white"} -->
+<div class="wp-block-group alignfull col-start-2 py-3 has-white-color has-text-color has-luminous-vivid-amber-background-color has-background has-link-color"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":145,"shouldSyncIcon":false} /-->
+
+<!-- wp:site-title /--></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":1552,"layout":{"type":"flex","orientation":"horizontal","justifyContent":"left"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

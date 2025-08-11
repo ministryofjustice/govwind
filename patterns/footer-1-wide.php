35a3a3b0dc6f30c5b23fc4d2b4c18a3208790cb2
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-08-11 14:13:38 +0100

    Lint & new footer patterns

--- patterns/footer-1-wide.php
+++ patterns/footer-1-wide.php
@@ -13,23 +13,26 @@
 ?>
 
 
-<!-- wp:group {"tagName":"footer","metadata":{"categories":["footer"],"patternName":"govwind/footer","name":"Footer (Crown copyright)"},"className":"col-start-2 my-3 flex justify-between flex-col sm:flex-row","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black","textColor":"white","layout":{"type":"flex","orientation":"vertical"}} -->
-    <footer class="wp-block-group col-start-2 my-3 flex justify-between flex-col sm:flex-row has-white-color has-black-background-color has-text-color has-background has-link-color"><!-- wp:group {"className":"lg:inline-block w-[100%] sm:w-[calc(100%-150px)] xl:w-[1056px] align-top","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-        <div class="wp-block-group lg:inline-block w-[100%] sm:w-[calc(100%-150px)] xl:w-[1056px] align-top"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-            <div class="wp-block-group"><!-- wp:heading {"className":"sr-only"} -->
-                <h2 class="wp-block-heading sr-only" id="footer-nav-heading">Footer links</h2>
-                <!-- /wp:heading -->
+<!-- wp:group {"tagName":"footer","metadata":{"categories":["footer"],"patternName":"govwind/footer-crown","name":"Footer (Crown copyright)"},"align":"full","style":{"spacing":{"padding":{"right":"0","left":"0","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"border":{"top":{"color":"var:preset|color|black","width":"5px"},"right":{"width":"0px","color":"var:preset|color|black","style":"none"},"bottom":{"width":"0px","color":"var:preset|color|black","style":"none"},"left":{"width":"0px","color":"var:preset|color|black","style":"none"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","gradient":"midnight","layout":{"type":"constrained"}} -->
+<footer class="wp-block-group alignfull has-white-color has-midnight-gradient-background has-text-color has-background has-link-color" style="border-top-color:var(--wp--preset--color--black);border-top-width:5px;border-right-color:var(--wp--preset--color--black);border-right-style:none;border-right-width:0px;border-bottom-color:var(--wp--preset--color--black);border-bottom-style:none;border-bottom-width:0px;border-left-color:var(--wp--preset--color--black);border-left-style:none;border-left-width:0px;padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:heading {"className":"sr-only"} -->
+<h2 class="wp-block-heading sr-only" id="footer-nav-heading">Footer links</h2>
+<!-- /wp:heading -->
 
-                <!-- wp:navigation {"ref":1511,"overlayMenu":"never","className":"[\u0026_.wp-block-navigation-item:not(:last-child)]:me-5","layout":{"type":"flex","orientation":"vertical"}} /-->
-            </div>
-            <!-- /wp:group -->
+<!-- wp:navigation {"ref":1511,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:group -->
 
-            <!-- wp:site-logo /-->
-        </div>
-    <!-- /wp:group -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"fontSize":"sm","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","flexWrap":"wrap"}} -->
+<div class="wp-block-group has-sm-font-size"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":1633,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail"><img src="https://hale.docker/playground/wp-content/uploads/sites/2/2025/08/MOJ_Lesser_Arms_Stacked_White-1-150x150.png" alt="" class="wp-image-1633"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
 
-    <!-- wp:paragraph {"className":"inline"} -->
-        <p>© Copyright Ministry of Justice</p>
-    <!-- /wp:paragraph -->
-</footer>
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|text"}}}},"textColor":"text"} -->
+<p class="has-text-align-center has-text-color has-link-color"><a href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/" data-type="link" data-id="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"><br>©Copyright</a> Ministry of Justice</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></footer>
 <!-- /wp:group -->

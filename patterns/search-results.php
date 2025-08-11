9c804275fb8c5ad9c50783a5625cc20a17d2b4ae
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-08-11 16:13:48 +0100

    Harmonized templates, adding contstrained

--- patterns/search-results.php
+++ patterns/search-results.php
@@ -11,38 +11,22 @@
  */
 ?>
 
-<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
-<main class="wp-block-group gw-main-content-area" style="margin-top: 0"><!-- wp:query-title {"type":"search"} /-->
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+<main
+	class="wp-block-group gw-main-content-area"
+	style="
+		margin-top: 0;
+		padding-top: var(--wp--preset--spacing--60);
+		padding-bottom: var(--wp--preset--spacing--60);
+	"
+>
+
+	<!-- wp:query-title {"type":"search"} /-->
 
 	<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search"} /-->
 	
-	<!-- wp:query {"queryId":1,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"align":"full","layout":{"type":"default"}} -->
-	<div class="wp-block-query alignfull"><!-- wp:post-template {"align":"full","layout":{"type":"default"}} -->
-	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:post-title {"isLink":true,"fontSize":"x-large"} /-->
+	<!-- wp:pattern {"slug":"govwing/template-query-loop"} /-->
 	
-	<!-- wp:post-date {"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"fontSize":"small"} /--></div>
-	<!-- /wp:group -->
-	<!-- /wp:post-template -->
-	
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:query-no-results -->
-	<!-- wp:paragraph -->
-	<p>Sorry, but nothing was found. Please try a search with different keywords.</p>
-	<!-- /wp:paragraph -->
-	<!-- /wp:query-no-results --></div>
-	<!-- /wp:group -->
-	
-	<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignwide"><!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<!-- wp:query-pagination-previous /-->
-	
-	<!-- wp:query-pagination-numbers /-->
-	
-	<!-- wp:query-pagination-next /-->
-	<!-- /wp:query-pagination --></div>
-	<!-- /wp:group --></div>
-	<!-- /wp:query -->
-	
-	<!-- wp:pattern {"slug":"govwing/more-posts"} /--></main>
-	<!-- /wp:group -->
+	<!-- wp:pattern {"slug":"govwing/more-posts"} /-->
+</main>
+<!-- /wp:group -->

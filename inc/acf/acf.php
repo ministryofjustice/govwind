206d94bbe5608ec29e9956a07fb55b6031401e31
adam <brown-a2@users.noreply.github.com>
2025-09-12 12:15:27 +0100

    Load document upload php file

--- inc/acf/acf.php
+++ inc/acf/acf.php
@@ -6,3 +6,4 @@ require get_template_directory() . '/inc/acf/admin/cpt-settings.php';
 //fields 
 require get_template_directory() . '/inc/acf/fields/field-groups.php';
 require get_template_directory() . '/inc/acf/fields/field-summary.php';
+require get_template_directory() . '/inc/acf/fields/field-document.php';

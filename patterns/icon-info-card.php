1d162ec8356c704d63145eb0598084c32cc37b4e
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-03-09 15:19:40 +0000

    Placeholders

--- patterns/icon-info-card.php
+++ patterns/icon-info-card.php
@@ -16,15 +16,15 @@
 <div class="wp-block-group has-background-secondary-background-color has-background" style="min-height:100px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
 
     <!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
-    <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)"><img src="/wp-content/themes/govwind/assets/images/placeholder.php?w=96&amp;h=96" alt="Placeholder image" style="object-fit:contain;width:48px;height:48px" /></figure>
+    <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)"><img src="<?php wb_placeholder_image(96, 96); ?>" alt="Placeholder image" style="object-fit:contain;width:48px;height:48px" /></figure>
     <!-- /wp:image -->
 
-    <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"base"} -->
-    <h3 class="wp-block-heading has-base-font-size" style="margin-top:0">Encrypted messaging</h3>
+    <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"base","placeholder":"Add your heading here."} -->
+    <h3 class="wp-block-heading has-base-font-size" style="margin-top:0"></h3>
     <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
-    <p style="font-size:16px">Thanks to Transport Layer Security (TLS) protocol, any communication is sent via end-to-end encryption.</p>
+    <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"placeholder":"Add your text here."} -->
+    <p style="font-size:16px"></p>
     <!-- /wp:paragraph -->
 
 </div>

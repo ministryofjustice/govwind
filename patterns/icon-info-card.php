2daacfb5b4f8b82a6fda68e0c45aa1fb22fd3d29
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-03-09 15:25:50 +0000

    Update font sizing

--- patterns/icon-info-card.php
+++ patterns/icon-info-card.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Icon Info Card
  * Slug: govwind/icon-info-card
@@ -12,19 +13,19 @@
  * @since Govwind 0.1.0
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100px"}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background-secondary-background-color has-background" style="min-height:100px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem">
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100px"},"typography":{"fontSize":"1rem"}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background-secondary-background-color has-background" style="min-height:100px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
 
     <!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
-    <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)"><img src="<?php wb_placeholder_image(96, 96); ?>" alt="Placeholder image" style="object-fit:contain;width:48px;height:48px" /></figure>
+    <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)"><img src="<?php wb_placeholder_image(96, 96); ?>" alt="" style="object-fit:contain;width:48px;height:48px" /></figure>
     <!-- /wp:image -->
 
-    <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"base","placeholder":"Add your heading here."} -->
+    <!-- wp:heading {"level":3,"placeholder":"Add your heading here.","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"base"} -->
     <h3 class="wp-block-heading has-base-font-size" style="margin-top:0"></h3>
     <!-- /wp:heading -->
 
-    <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"placeholder":"Add your text here."} -->
-    <p style="font-size:16px"></p>
+    <!-- wp:paragraph {"placeholder":"Add your text here."} -->
+    <p></p>
     <!-- /wp:paragraph -->
 
 </div>

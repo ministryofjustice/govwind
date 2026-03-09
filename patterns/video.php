5492975bd8f565354777e1b732a98c637476e2d0
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-03-09 10:54:12 +0000

    Video with reveal pattern

---
+++ patterns/video.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Video with reveal
+ * Slug: govwind/video
+ * Categories: page
+ * Inserter: yes
+ * Description: Video with reveal that can be used for transcript
+ */
+?>
+<!-- wp:embed {"url":"https://www.youtube.com/watch?v=ih7DZk-9US8","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio" style="margin-bottom:var(--wp--preset--spacing--50)"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?v=ih7DZk-9US8
+</div></figure>
+<!-- /wp:embed -->
+
+<!-- wp:wb-blocks/reveal {"revealTitle":"View video transcript","revealClassName":"wp-block-wb-blocks-reveal"} -->
+<!-- wp:paragraph -->
+<p>Video transcripts are an important accessibility feature because they provide a written version of the spoken content in a video. This allows people who are deaf or hard of hearing to read the dialogue and understand the information being presented. Transcripts also help people who prefer reading over listening, or who may be watching in environments where audio isn’t available.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Providing transcripts also improves usability for a wider audience. They make it easier to search for specific information within a video, support translation into other languages, and help people with cognitive or learning differences follow along at their own pace.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:wb-blocks/reveal -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+

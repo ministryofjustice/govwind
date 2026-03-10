e4769faa436102de53a69376e9e7e4ef7d4aaf75
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-03-10 09:40:01 +0000

    PR amends - video pattern

--- patterns/video-with-reveal.php
+++ patterns/video-with-reveal.php
@@ -4,6 +4,7 @@
  * Slug: govwind/video-with-reveal
  * Categories: page
  * Inserter: yes
+ * Keywords: video, youtube, vimeo, embed, transcript
  * Description: Video with reveal that can be used for transcripts
  */
 ?>
@@ -13,12 +14,5 @@ https://www.youtube.com/watch?v=ih7DZk-9US8
 </div></figure>
 <!-- /wp:embed -->
 
-<!-- wp:wb-blocks/reveal {"revealTitle":"View video transcript","revealClassName":"wp-block-wb-blocks-reveal"} -->
-<!-- wp:paragraph -->
-<p>Video transcripts are an important accessibility feature because they provide a written version of the spoken content in a video. This allows people who are deaf or hard of hearing to read the dialogue and understand the information being presented. Transcripts also help people who prefer reading over listening, or who may be watching in environments where audio isn’t available.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Providing transcripts also improves usability for a wider audience. They make it easier to search for specific information within a video, support translation into other languages, and help people with cognitive or learning differences follow along at their own pace.</p>
-<!-- /wp:paragraph -->
+<!-- wp:wb-blocks/reveal {"revealTitle":"","revealClassName":"wp-block-wb-blocks-reveal"} -->
 <!-- /wp:wb-blocks/reveal -->

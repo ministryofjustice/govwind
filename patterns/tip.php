30525b38c2f17d327c4398e2ce0d6207c411b66f
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-03-10 15:19:14 +0000

    TIP Pattern

---
+++ patterns/tip.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Tip
+ * Slug: govwind/tip
+ * Categories: page
+ * Description: A tip or impportant piece of content
+ * Keywords: tip
+ *
+ */
+?>
+<!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|cjsm-gold"},"right":{"color":"var:preset|color|cjsm-gold"},"bottom":{"color":"var:preset|color|cjsm-gold"},"left":{"color":"var:preset|color|cjsm-gold","width":"4px"}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"cjsm-gold-muted","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-cjsm-gold-muted-background-color has-background" style="border-top-color:var(--wp--preset--color--cjsm-gold);border-right-color:var(--wp--preset--color--cjsm-gold);border-bottom-color:var(--wp--preset--color--cjsm-gold);border-left-color:var(--wp--preset--color--cjsm-gold);border-left-width:4px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+
+<!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+
+<!-- wp:heading {"style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"fontSize":"lg"} -->
+    <h2 class="wp-block-heading has-lg-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">TIP</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non orci vitae nibh luctus fermentum. Integer feugiat, nibh sit amet tristique volutpat, neque elit convallis lacus, at pretium mi nisi sit amet lorem. Curabitur at lacus ac velit suscipit ultrices vitae vel arcu.', 'govwind' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+

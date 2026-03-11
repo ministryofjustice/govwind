d6e3a25e6ad7efef9e8f3a868ef38e4089313ac6
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-03-11 09:53:08 +0000

    Tip pattern

--- patterns/tip.php
+++ patterns/tip.php
@@ -11,14 +11,19 @@
 <!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|cjsm-gold"},"right":{"color":"var:preset|color|cjsm-gold"},"bottom":{"color":"var:preset|color|cjsm-gold"},"left":{"color":"var:preset|color|cjsm-gold","width":"4px"}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"cjsm-gold-muted","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-cjsm-gold-muted-background-color has-background" style="border-top-color:var(--wp--preset--color--cjsm-gold);border-right-color:var(--wp--preset--color--cjsm-gold);border-bottom-color:var(--wp--preset--color--cjsm-gold);border-left-color:var(--wp--preset--color--cjsm-gold);border-left-width:4px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
 
-<!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+    <div class="wp-block-group">
+        <!-- wp:image {"width":"24px","height":"24px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+        <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)">
+            <img src="<?php wb_placeholder_image(96, 96); ?>" alt="" style="object-fit:contain;width:24px;height:24px" />
+        </figure>
+        <!-- /wp:image -->
+        <!-- wp:heading {"fontSize":"lg"} -->
+        <h2 class="wp-block-heading has-lg-font-size">TIP</h2>
+        <!-- /wp:heading --></div>
+    <!-- /wp:group -->
 
-<!-- wp:heading {"style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"fontSize":"lg"} -->
-    <h2 class="wp-block-heading has-lg-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><?php esc_html_e( 'TIP', 'govwind' ); ?></h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p><?php esc_html_e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non orci vitae nibh luctus fermentum. Integer feugiat, nibh sit amet tristique volutpat, neque elit convallis lacus, at pretium mi nisi sit amet lorem. Curabitur at lacus ac velit suscipit ultrices vitae vel arcu.', 'govwind' ); ?></p>
-<!-- /wp:paragraph --></div>
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non orci vitae nibh luctus fermentum. Integer feugiat, nibh sit amet tristique volutpat, neque elit convallis lacus, at pretium mi nisi sit amet lorem. Curabitur at lacus ac velit suscipit ultrices vitae vel arcu.</p>
+    <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
-

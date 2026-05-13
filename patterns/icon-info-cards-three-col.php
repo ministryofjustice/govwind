df72c08db2ebafd56c0a56c7be8ccb4bad8a9ab0
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-05-13 11:43:42 +0100

    Add icon block to patterns, adjust default backgrounds and font colours

--- patterns/icon-info-cards-three-col.php
+++ patterns/icon-info-cards-three-col.php
@@ -17,17 +17,13 @@
 
     <!-- wp:column -->
     <div class="wp-block-column">
-        <!-- wp:group {"metadata":{"categories":["page"],"patternName":"govwind/icon-info-card","name":"Icon Info Card"},"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100%"},"typography":{"fontSize":"1rem"}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
-        <div class="wp-block-group has-background-secondary-background-color has-background" style="min-height:100%;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
+        <!-- wp:group {"metadata":{"categories":["page"],"patternName":"govwind/icon-info-card","name":"Icon Info Card"},"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100%"},"typography":{"fontSize":"1rem"}},"backgroundColor":"greyscale-B","layout":{"type":"constrained"}} -->
+        <div class="wp-block-group has-greyscale-b-background-color has-background" style="min-height:100%;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
 
-            <!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
-            <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)">
-                <img src="https://hale.docker/cjsm/wp-content/themes/govwind/assets/images/placeholder.php?w=96&amp;h=96&amp;t=" alt="" style="object-fit:contain;width:48px;height:48px" />
-            </figure>
-            <!-- /wp:image -->
+           <!-- wp:wb-blocks/icon {"size":2.5} /-->
 
-            <!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"3-xl"} -->
-            <h2 class="wp-block-heading has-3-xl-font-size" style="margin-top:0">Add your heading here.</h2>
+            <!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"lg"} -->
+            <h2 class="wp-block-heading has-lg-font-size" style="margin-top:0">Add your heading here.</h2>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph -->
@@ -40,17 +36,13 @@
 
     <!-- wp:column -->
     <div class="wp-block-column">
-        <!-- wp:group {"metadata":{"categories":["page"],"patternName":"govwind/icon-info-card","name":"Icon Info Card"},"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100%"},"typography":{"fontSize":"1rem"}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
-        <div class="wp-block-group has-background-secondary-background-color has-background" style="min-height:100%;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
+        <!-- wp:group {"metadata":{"categories":["page"],"patternName":"govwind/icon-info-card","name":"Icon Info Card"},"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100%"},"typography":{"fontSize":"1rem"}},"backgroundColor":"greyscale-B","layout":{"type":"constrained"}} -->
+        <div class="wp-block-group has-greyscale-b-background-color has-background" style="min-height:100%;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
 
-            <!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
-            <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)">
-                <img src="https://hale.docker/cjsm/wp-content/themes/govwind/assets/images/placeholder.php?w=96&amp;h=96&amp;t=" alt="" style="object-fit:contain;width:48px;height:48px" />
-            </figure>
-            <!-- /wp:image -->
+           <!-- wp:wb-blocks/icon {"size":2.5} /-->
 
-            <!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"3-xl"} -->
-            <h2 class="wp-block-heading has-3-xl-font-size" style="margin-top:0">Add your heading here.</h2>
+            <!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"lg"} -->
+            <h2 class="wp-block-heading has-lg-font-size" style="margin-top:0">Add your heading here.</h2>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph -->
@@ -63,17 +55,13 @@
 
     <!-- wp:column -->
     <div class="wp-block-column">
-        <!-- wp:group {"metadata":{"categories":["page"],"patternName":"govwind/icon-info-card","name":"Icon Info Card"},"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100%"},"typography":{"fontSize":"1rem"}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
-        <div class="wp-block-group has-background-secondary-background-color has-background" style="min-height:100%;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
+        <!-- wp:group {"metadata":{"categories":["page"],"patternName":"govwind/icon-info-card","name":"Icon Info Card"},"style":{"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"}},"dimensions":{"minHeight":"100%"},"typography":{"fontSize":"1rem"}},"backgroundColor":"greyscale-B","layout":{"type":"constrained"}} -->
+        <div class="wp-block-group has-greyscale-b-background-color has-background" style="min-height:100%;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;font-size:1rem">
 
-            <!-- wp:image {"width":"48px","height":"48px","scale":"contain","sizeSlug":"large","className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
-            <figure class="wp-block-image size-large is-resized is-style-default" style="margin-bottom:var(--wp--preset--spacing--50)">
-                <img src="https://hale.docker/cjsm/wp-content/themes/govwind/assets/images/placeholder.php?w=96&amp;h=96&amp;t=" alt="" style="object-fit:contain;width:48px;height:48px" />
-            </figure>
-            <!-- /wp:image -->
+            <!-- wp:wb-blocks/icon {"size":2.5} /-->
 
-            <!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"3-xl"} -->
-            <h2 class="wp-block-heading has-3-xl-font-size" style="margin-top:0">Add your heading here.</h2>
+            <!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"lg"} -->
+            <h2 class="wp-block-heading has-lg-font-size" style="margin-top:0">Add your heading here.</h2>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph -->

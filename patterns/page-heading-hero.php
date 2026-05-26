ba24564c3f2939cdeee6bfeb9580dc71805bf38b
Malcolm.Butler <malcolm.butler@digital.justice.gov.uk>
2026-05-26 15:35:29 +0100

    corrected image handling

--- patterns/page-heading-hero.php
+++ patterns/page-heading-hero.php
@@ -9,9 +9,9 @@
 ?>
 <!-- wp:group {"metadata":{"categories":["header","featured"],"patternName":"govwind/page-heading-hero","name":"Page Heading Hero"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|greyscale-A"},":hover":{"color":{"text":"var:preset|color|background"}}}}},"backgroundColor":"colour-Z","textColor":"greyscale-A","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-greyscale-a-color has-colour-z-background-color has-text-color has-background has-link-color"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%","style":{"spacing":{"padding":{"bottom":"2rem"}}}} -->
-<div class="wp-block-column" style="padding-bottom:2rem;flex-basis:66.66%"><!-- wp:group {"metadata":{"name":"Breadcrumb"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%","style":{"spacing":{"padding":{"bottom":"2.1rem"}}}} -->
+<div class="wp-block-column" style="padding-bottom:2.1rem;flex-basis:66.66%"><!-- wp:group {"metadata":{"name":"Breadcrumb"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:boldblocks/breadcrumb-block /--></div>
 <!-- /wp:group -->
 
@@ -28,15 +28,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"33.33%"} -->
-<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"id":1134,"sizeSlug":"full","linkDestination":"none","className":"hidden md:block!","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}}} -->
-<figure class="wp-block-image size-full has-custom-border hidden md:block! hidden md:block! [&>img]:absolute [&>img]:h-full! [&>img]:max-w-max!">
-	<img
-		src="<?php gw_placeholder_image(600, 600); ?>"
-		alt=""
-		class="wp-image-1134"
-		style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px"
-	/>
-</figure>
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"id":1157,"sizeSlug":"full","linkDestination":"none","className":"hidden md:block! md:relative left-[var(\u002d\u002dwp\u002d\u002dstyle\u002d\u002droot\u002d\u002dpadding-left)] [\u0026\u003eimg]:h-full! [\u0026\u003eimg]:w-full! [\u0026\u003eimg]:max-w-max! hidden md:block! [\u0026\u003eimg]:object-cover [\u0026\u003eimg]:object-left","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}}} -->
+<figure class="wp-block-image size-full has-custom-border hidden md:block! md:relative left-[var(--wp--style--root--padding-left)] [&>img]:h-full! [&>img]:w-full! [&>img]:max-w-max! [&>img]:object-cover [&>img]:object-left"><img src="https://hale.docker/cjsm/wp-content/uploads/sites/56/2026/05/CJSM-abstract-1.svg" alt="" class="wp-image-1157" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>

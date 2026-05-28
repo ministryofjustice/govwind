cdeddb7615b10199b6eeb63a1bf35407ada0bf36
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2026-05-28 10:45:47 +0100

    image src

--- patterns/page-heading-hero.php
+++ patterns/page-heading-hero.php
@@ -29,7 +29,7 @@
 
 <!-- wp:column {"width":"33.33%"} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"id":1157,"sizeSlug":"full","linkDestination":"none","className":"hidden md:block! md:relative left-[var(\u002d\u002dwp\u002d\u002dstyle\u002d\u002droot\u002d\u002dpadding-left)] [\u0026\u003eimg]:h-full! [\u0026\u003eimg]:w-full! [\u0026\u003eimg]:max-w-max! hidden md:block! [\u0026\u003eimg]:object-cover [\u0026\u003eimg]:object-left","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}}} -->
-<figure class="wp-block-image size-full has-custom-border hidden md:block! md:relative left-[var(--wp--style--root--padding-left)] [&>img]:h-full! [&>img]:w-full! [&>img]:max-w-max! [&>img]:object-cover [&>img]:object-left"><img src="https://hale.docker/cjsm/wp-content/uploads/sites/56/2026/05/CJSM-abstract-1.svg" alt="" class="wp-image-1157" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px"/></figure>
+<figure class="wp-block-image size-full has-custom-border hidden md:block! md:relative left-[var(--wp--style--root--padding-left)] [&>img]:h-full! [&>img]:w-full! [&>img]:max-w-max! [&>img]:object-cover [&>img]:object-left"><img src="<?php gw_placeholder_image(600, 600); ?>" alt="" class="wp-image-1134" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
